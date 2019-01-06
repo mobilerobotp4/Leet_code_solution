3679e860a18108ff8bfedeffd8e27ac6b830352a
Saswati Bhattacharjee <36052453+mobilerobotp4@users.noreply.github.com>
2019-01-06 00:09:38 -0500

    Update Q34.java

--- Q34.java
+++ Q34.java
@@ -1,4 +1,6 @@
-/*Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+/*Find First and Last Position of Element in Sorted Array
+
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
 Example 1:
 
 Input: nums = [5,7,7,8,8,10], target = 8
@@ -8,44 +10,35 @@ Input: nums = [5,7,7,8,8,10], target = 6
 Output: [-1,-1]
  */
 
-package Leetcode;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Q34 {
     public static void main(String args[]){
-        int []arr={5,7,7,8,8,8,10};
-        int t=8;
+        int []arr={5,7,7,8,8,8,10}; //Input Array
+        int t=8; //Target Values
     Solution34 obj34=new Solution34();
     System.out.println(Arrays.toString(obj34.searchRange(arr,t)));
     }
 }
 class Solution34{
     public int[] searchRange(int[] nums, int target) {
-        //ArrayList<Integer> index=new ArrayList<>();
-        int[]index={-1,-1};
+       
+        int[]index={-1,-1};// Intialize with {-1,-1} because if the target is not present in the given array it returned (-1,-1)
         int old_end_index=-1;
         boolean found_startindex=false;
         int[]a=new int[2];
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target){
                 if(!found_startindex){
-                    index[0]=i;
+                    index[0]=i; // When 1st target index found
                     found_startindex=true;
                 }
                 old_end_index=i;
 
             }
         }
-        index[1]=old_end_index;
-       /* Integer[] index_arr = new Integer[index.size()];
-        index_arr = index.toArray(index_arr);*/
-       /* if(!found_startindex){
-            index[0]=-1;
-            index[1]=-1;
-        }*/
-        //System.out.println(index);
+        index[1]=old_end_index; //Assign last target index
+      
       return index;
     }
 }
