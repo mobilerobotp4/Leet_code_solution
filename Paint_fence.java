d93ff8392ddb40acaa0b4000d5cf5474b5a221ef
Saswati Bhattacharjee <36052453+mobilerobotp4@users.noreply.github.com>
2019-01-08 13:18:13 -0500

    Update Paint_fence.java

--- Paint_fence.java
+++ Paint_fence.java
@@ -1,4 +1,15 @@
-package Leetcode;
+/*There is a fence with n posts, each post can be painted with one of the k colors. You have to paint all the posts such that no 
+more than two adjacent fence posts have the same color. Return the total number of ways you can paint the fence.
+
+Approach: Dynamic Programming
+
+Key Idea: To solve the problem find this relation: f(n) = (k-1)(f(n-1)+f(n-2))
+
+Explanation: Assuming there are 3 posts, if the first one and the second one has the same color, then the third one has k-1 options. 
+The first and second together has k options.If the first and the second do not have same color, the total is k * (k-1), then the third 
+one has k options. 
+Therefore, f(3) = (k-1)*k + k*(k-1)*k = (k-1)(k+k*k)
+*/
 
 import java.util.Scanner;
 
