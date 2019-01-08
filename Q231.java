3ad39fd9e3929184a2c3a95fb8057f4b18a55a90
Saswati Bhattacharjee <36052453+mobilerobotp4@users.noreply.github.com>
2019-01-08 13:43:03 -0500

    Add files via upload

---
+++ Q231.java
@@ -0,0 +1,44 @@
+/*
+Given an integer, write a function to determine if it is a power of two.
+
+Example 1:
+
+Input: 1
+Output: true
+Explanation: 20 = 1
+Example 2:
+
+Input: 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+
+Input: 218
+Output: false
+ */
+package Leetcode;
+
+public class Q231 {
+    public static void main(String args[]){
+        Solution231 obj231=new Solution231();
+        System.out.println(obj231.isPowerOfTwo(32));
+
+    }
+}
+class Solution231{
+    public boolean isPowerOfTwo(int n) {
+
+        if(n<1)
+            return false;
+        while(n>2)
+        {
+            if(n%2!=0) {
+                return false;
+
+            }
+            n = n / 2;
+        }
+        return true;
+
+    }
+}
