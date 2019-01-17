1ade1166297a92c2cb697116fc48ae4a2dbe3030
Saswati Bhattacharjee <36052453+mobilerobotp4@users.noreply.github.com>
2019-01-17 16:35:55 -0500

    Update Q41.java

--- Q41.java
+++ Q41.java
@@ -1,14 +1,29 @@
-package Leetcode;
 
+/*
+Given an unsorted integer array, find the smallest missing positive integer.
+
+Example 1:
+
+Input: [1,2,0]
+Output: 3
+Example 2:
+
+Input: [3,4,-1,1]
+Output: 2
+Example 3:
+
+Input: [7,8,9,11,12]
+Output: 1
+*/
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Q41 {
      public static void main(String args[]){
-        // int[]numer={2,1,0};
-         //int[]numer={3,4,-1,1};
-         int[]numer={ 7,8,9,11,12};
+        // int[]numer={2,1,0}; Test Case:1
+         //int[]numer={3,4,-1,1}; Test Case:2
+         int[]numer={ 7,8,9,11,12}; //Test Case:3
          Solution41 obj41=new Solution41();
          System.out.println(obj41.firstMissingPositive(numer));
 
@@ -19,9 +34,9 @@ class Solution41{
         Map<Integer,Boolean> map=new HashMap();
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],true);
-            System.out.println(map.get(i));
+            
         }
-        System.out.println(map);
+        
         int result=1;
         while(result<=Integer.MAX_VALUE){
             if(map.containsKey(result)){
