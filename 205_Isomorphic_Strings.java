6cfac2973e95586c5a69f28e68bf2f6cfe027bcb
Saswati Bhattacharjee <36052453+mobilerobotp4@users.noreply.github.com>
2020-11-07 13:51:16 +0530

    Update 205_Isomorphic_Strings.java

--- 205_Isomorphic_Strings.java
+++ 205_Isomorphic_Strings.java
@@ -53,3 +53,25 @@ class Solution {
         
     }
 }
+
+// 2nd Solution (Space Efficient)
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length()!=t.length()){
+           return false; 
+        } 
+        int[] arr1 =  new int[128];
+        int[] arr2 = new int[128];
+        for(int i=0; i<s.length(); i++){
+            if(arr1[s.charAt(i)]!= arr2[t.charAt(i)]){
+                return false;
+            }
+            arr1[s.charAt(i)]= i+1;
+            arr2[t.charAt(i)]=i+1;
+                
+        }
+        return true;
+        
+    }
+}
